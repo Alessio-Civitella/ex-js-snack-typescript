79db9d819de6c260efa07760120818eb6890838c
Alessio-Civitella <alessio.civitella19@gmail.com>
2025-06-03 10:25:05 +0200

    snack 2

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,24 @@ function stampaValore(dato: any): void {
   } else {
     console.log("Tipo non supportato");
   }
-}
+};
+
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: number[];
+};
+
+
+const mario: Dipendente = {
+  nome: "Mario",
+  cognome: "Rossi",
+  annoNascita: 1985,
+  sesso: "m",
+  anniDiServizio: [2014, 2015, 2017, 2018]
+};
+
+console.log(mario);
+
